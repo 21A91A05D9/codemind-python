5efe7db2125df92c6254dd2037bef0aaa3ec14d2
21A91A05D9 <97424618+21A91A05D9@users.noreply.github.com>
2022-06-06 01:13:33 +0530

    a=input() a=a.lower() a=a.split() a=''.join(a) a=set(a) a=list(a) a.sort() a=''.join(a) print(a)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -2,12 +2,8 @@ a=input()
 a=a.lower()
 a=a.split()
 a=''.join(a)
-c=0;s=""
-for i in a:
-    c=a.count(i)
-    if c==1:
-        s+=i
-s=list(s)
-s.sort()
-s=''.join(s)
-print(s)
+a=set(a)
+a=list(a)
+a.sort()
+a=''.join(a)
+print(a)
