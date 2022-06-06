5f986d5641164506e67bca043815665ab17126b3
21A91A05D9 <97424618+21A91A05D9@users.noreply.github.com>
2022-06-06 18:16:48 +0530

    a=input() a=a.lower() a=a.split() x={'a','e','i','o','u'} l=[];ma=0 for i in a:     c=0     for j in i:         if j in x:             c+=1     if c>=ma:         ma=c         l.append(ma) print(l.count(ma))

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,15 @@
+a=input()
+a=a.lower()
+a=a.split()
+x={'a','e','i','o','u'}
+l=[];ma=0
+for i in a:
+    c=0
+    for j in i:
+        if j in x:
+            c+=1
+    if c>=ma:
+        ma=c
+        l.append(ma)
+print(l.count(ma))
+        
