b35133b8a93fced6bd592226fdbd8a05aa3dd937
21A91A05D9 <97424618+21A91A05D9@users.noreply.github.com>
2022-07-16 09:29:43 +0530

    n=int(input()) s=list(map(int,input().split())) c=0;v=0 for i in range(0,n-2,+2):     if s[i]<s[i+1] and s[i+1]>s[i+2]:         c+=1     elif s[i]>s[i+1] and s[i+1]<s[i+2]:         c+=1     else:         v=1         break if v==0:     print("yes") else:     print("no")

--- Wave_array.py
+++ Wave_array.py
@@ -1,12 +1,15 @@
-n=input()
-a=list(map(int,input().split()))
+n=int(input())
+s=list(map(int,input().split()))
 c=0;v=0
-for i in range(len(a)-1):
-    if a[i]>a[i+1]:
-        v+=1
-    elif a[i]<a[i+1]:
+for i in range(0,n-2,+2):
+    if s[i]<s[i+1] and s[i+1]>s[i+2]:
         c+=1
-if v==c or v-1==c or v==c-1:
+    elif s[i]>s[i+1] and s[i+1]<s[i+2]:
+        c+=1
+    else:
+        v=1
+        break
+if v==0:
     print("yes")
 else:
     print("no")
